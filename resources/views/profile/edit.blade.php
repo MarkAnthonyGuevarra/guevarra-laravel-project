58fc2280f590c44d5edfdcb6a304f0002d59fcf2
Mark Anthony Guevarra <guevarramark89@gmail.com>
2024-05-27 10:48:40 -0230

    Integrate Nice Admin

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,4 +1,222 @@
 <x-app-layout>
+
+<div class="pagetitle">
+      <h1>Profile</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">{{ __('Dashboard') }}</a></li>
+          <li class="breadcrumb-item">{{ __('Users') }}</li>
+          <li class="breadcrumb-item active">{{ __('Profile') }}</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section profile">
+      <div class="row">
+        <div class="col-xl-4">
+
+          <div class="card">
+            <div class="card-body profile-card pt-4 d-flex flex-column align-items-center">
+
+              <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+              <h2>{{ Auth::user()->name }}</h2>
+              <h3>Web Designer</h3>
+              <div class="social-links mt-2">
+                <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-xl-8">
+
+          <div class="card">
+            <div class="card-body pt-3">
+              <!-- Bordered Tabs -->
+              <ul class="nav nav-tabs nav-tabs-bordered">
+
+                <li class="nav-item">
+                  <button class="nav-link" data-bs-toggle="tab" data-bs-target="#update-profile">{{ __('Edit Profile') }}</button>
+                </li>
+
+                <li class="nav-item">
+                  <button class="nav-link" data-bs-toggle="tab" data-bs-target="#update-password">{{ __('Update Password') }}</button>
+                </li>
+
+                <li class="nav-item">
+                  <button class="nav-link" data-bs-toggle="tab" data-bs-target="#delete-user">{{ __('Delete Account') }}</button>
+                </li>
+
+              </ul>
+              <div class="tab-content pt-2">
+
+                <div class="tab-pane fade show active profile-overview" id="update-profile">
+                  <!-- Profile Edit Form -->
+                  <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+                    @csrf
+                  </form>
+                  <form method="post" action="{{ route('profile.update') }}">
+                    @csrf
+                    @method('patch')
+
+                    <div class="row mb-3">
+                      <label for="Name" class="col-md-4 col-lg-3 col-form-label">{{ __('Name') }}</label>
+                      <div class="col-md-8 col-lg-9">
+                      <input name="name" type="email" class="form-control" id="name" value="{{ Auth::user()->name }}" required autocomplete="name"/>
+                        <x-input-error class="mt-2" :messages="$errors->get('email')" class="invalid-feedback" />
+                      </div>
+                    </div>
+
+                    <div class="row mb-3">
+                      <label for="Email" class="col-md-4 col-lg-3 col-form-label">{{ __('Email') }}</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="email" type="email" class="form-control" id="Email" value="{{ Auth::user()->email }}" required autocomplete="username"/>
+                        <x-input-error class="mt-2" :messages="$errors->get('email')" class="invalid-feedback" />
+                      </div>
+                      
+                        @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
+                            <div>
+                                <p class="text-sm mt-2 text-gray-800 dark:text-gray-200">
+                                    {{ __('Your email address is unverified.') }}
+
+                                    <button form="send-verification" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
+                                        {{ __('Click here to re-send the verification email.') }}
+                                    </button>
+                                </p>
+
+                                @if (session('status') === 'verification-link-sent')
+                                    <p class="mt-2 font-medium text-sm text-green-600 dark:text-green-400">
+                                        {{ __('A new verification link has been sent to your email address.') }}
+                                    </p>
+                                @endif
+                            </div>
+                        @endif
+                    </div>
+
+                    <div class="text-center">
+                      <button form="send-verification" type="submit" class="btn btn-primary">{{ __('Save Changes') }}</button>
+
+                        @if (session('status') === 'profile-updated')
+                            <p
+                                x-data="{ show: true }"
+                                x-show="show"
+                                x-transition
+                                x-init="setTimeout(() => show = false, 2000)"
+                                class="text-sm text-gray-600 dark:text-gray-400"
+                            >{{ __('Saved.') }}</p>
+                        @endif
+                    </div>
+                  </form><!-- End Profile Edit Form -->
+
+                </div>
+
+                <div class="tab-pane fade pt-3" id="update-password">
+                  <!-- Change Password Form -->
+                  <form method="post" action="{{ route('password.update') }}">
+                    @csrf
+                    @method('put')
+
+                    <div class="row mb-3">
+                      <label for="update_password_current_password" class="col-md-4 col-lg-3 col-form-label">Current Password</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="current_password" type="password" class="form-control" id="update_password_current_password" autocomplete="current-password"/>
+                        <x-input-error :messages="$errors->updatePassword->get('current_password')" class="invalid-feedback" />
+                      </div>
+                    </div>
+
+                    <div class="row mb-3">
+                      <label for="update_password_password" class="col-md-4 col-lg-3 col-form-label">New Password</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="password" type="password" class="form-control" id="update_password_password" autocomplete="new-password">
+                        <x-input-error :messages="$errors->updatePassword->get('password')" class="invalid-feedback" />
+                      </div>
+                    </div>
+
+                    <div class="row mb-3">
+                      <label for="update_password_password_confirmation" class="col-md-4 col-lg-3 col-form-label">Confirm Password</label>
+                      <div class="col-md-8 col-lg-9">
+                        <input name="password_confirmation" type="password" class="form-control" id="update_password_password_confirmation">
+                        <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="invalid-feedback" />
+                      </div>
+                    </div>
+
+                    <div class="text-center">
+                        <button type="submit" class="btn btn-primary">{{ __('Confirm Password') }}</button>
+
+                        @if (session('status') === 'password-updated')
+                            <p
+                                x-data="{ show: true }"
+                                x-show="show"
+                                x-transition
+                                x-init="setTimeout(() => show = false, 2000)"
+                                class="text-sm text-gray-600 dark:text-gray-400"
+                            >{{ __('Saved.') }}</p>
+                        @endif
+                    </div>
+                  </form><!-- End Change Password Form -->
+
+                </div>
+
+                <div class="tab-pane fade pt-3" id="delete-user">
+                <x-danger-button
+x-data=""
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-user-deletion')"
+    >{{ __('Delete Account') }}</x-danger-button>
+
+    <x-modal name="confirm-user-deletion" :show="$errors->userDeletion->isNotEmpty()" focusable>
+        <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
+            @csrf
+            @method('delete')
+
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                {{ __('Are you sure you want to delete your account?') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
+            </p>
+
+            <div class="mt-6">
+                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+
+                <x-text-input
+                    id="password"
+                    name="password"
+                    type="password"
+                    class="mt-1 block w-3/4"
+                    placeholder="{{ __('Password') }}"
+                />
+
+                <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
+            </div>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-danger-button class="ms-3">
+                    {{ __('Delete Account') }}
+                </x-danger-button>
+            </div>
+        </form>
+    </x-modal>
+
+                </div>
+
+              </div><!-- End Bordered Tabs -->
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
     <x-slot name="header" id="header" class="header fixed-top d-flex align-items-center">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Profile') }}
